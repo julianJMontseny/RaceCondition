d6204f76d06282a4cd641d8613d9f7694fef25c4
mikelsanvi <mikelsanvi@gmail.com>
2021-10-05 09:33:19 +0200

    AddToListExample

---
+++ src/main/java/org/iesfm/racecondition/lista/AddToListTask.java
@@ -0,0 +1,23 @@
+package org.iesfm.racecondition.lista;
+
+import java.util.List;
+
+public class AddToListTask implements Runnable {
+    private List<Integer> lista;
+    private int n;
+
+    public AddToListTask(List<Integer> lista, int n) {
+        this.lista = lista;
+        this.n = n;
+    }
+
+    @Override
+    public void run() {
+
+        // H1  syncrhonized(lista), adquiere(lista)  for { add ... add }, libera(lista)
+        // H2      syncrhonized(lista) _________________________________________________ adquiere(lista)
+        for (int i = 0; i < n; i++) {
+            lista.add(i);
+        }
+    }
+}
---
+++ src/main/java/org/iesfm/racecondition/lista/Main.java
@@ -0,0 +1,39 @@
+package org.iesfm.racecondition.lista;
+
+import org.iesfm.racecondition.increment.IncrementTask;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    private static Logger log = LoggerFactory.getLogger(
+            org.iesfm.racecondition.increment.Main.class
+    );
+
+    public static void main(String[] args) {
+        // Ahora la lista es sincronizada, es decir, es thread safe
+        // La ventaja es que es thread safe
+        // La desventaje es que es más lenta
+        List<Integer> lista = Collections.synchronizedList(new ArrayList<>());
+        List<Thread> threads = new LinkedList<>();
+        for (int i = 0; i < 100; i++) {
+            Thread t = new Thread(new AddToListTask(lista, 100000));
+            t.start();
+            threads.add(t);
+        }
+
+        for (Thread t : threads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        log.info("Hay " + lista.size() + " elementos en la lista");
+    }
+}
